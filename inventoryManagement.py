1405ba9279b4ab4b488121308c404e9512a7f4c2
kevin-oeste <oestekevin@gmail.com>
2024-05-31 13:04:31 -0400

    Create inventoryManagement.py
    Classes and functions to be used in the inventory management system

---
+++ inventoryManagement.py
@@ -0,0 +1,9 @@
+class Item:
+  #Primary Key (changed name from productId to itemId)
+  int itemId
+  string itemName
+  string itemDescription
+  string itemCategory
+  int stock
+  double price
+  
