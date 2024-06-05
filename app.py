e614f41f71e37605b16b186923becd488cf1ba45
kevin-oeste <oestekevin@gmail.com>
2024-06-05 12:57:57 -0400

    Update app.py
    miniscule update to itemAdd, imported inventoryManagement, made sure to call app.run() at the end

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request
 import pymongo 
 from pymongo import MongoClient as mc
+import inventoryManagement as im
+
+#unsure if I will need these in the future, database should handle it
+inventory = []
+shoppingCart = []
 
 #connect to database
+#unfinished
 try:
     uri = "mongodb+srv://oestekevin:nbFFcWwiS4OYx5k4@sportsequipmentserver.xsphcuo.mongodb.net/?retryWrites=true&w=majority&appName=sportsEquipmentServer"
     client = mc(uri)
@@ -19,21 +25,36 @@ except Exception as e:
 
 app = Flask(__name__)
 
+#home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#displays addItem webpage
 @app.route('/addItem')
 def addItem():
     return render_template('addItem.html')
 
+#unfinished
+#Actually adds the item to the database
 @app.route('/itemAdd', methods = ['POST', 'GET'])
 def itemAdd():
     if request.method == 'POST':
         try:
+            #get parameters
             name = request.form['itemName']
             desc = request.form['itemDescription']
             cat = request.form['itemCategory']
             stock = request.form['itemStock']
             price = request.form['itemPrice']
+            #calls parameterized constructor
+            inventory.append(im.Item(name, desc, cat, stock, price))
+        except:
+            msg = "Error: Item could not be added.\n"
+        finally:
+            return render_template('result.html', msg = msg)
+
+#Set debug to true for now, but set it to false when we turn it in
+if __name__ == "__main__":
+    app.run(debug = True)
             
