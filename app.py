46ddd2f61fd9d14b67953b06d42240af71838530
kevin-oeste <oestekevin@gmail.com>
2024-06-05 12:10:50 -0400

    Update and rename app.py to setup.py
    Trying to set up mongoDB

--- app.py
+++
@@ -1,5 +0,0 @@
-#Create a basic interface using Flask
-
-from flask import Flask, render_template, request
-#temporary
-import sqlite3 as sql
---
+++ setup.py
@@ -0,0 +1,44 @@
+#Setup database with MongoDB
+
+#from flask import Flask, render_template, request
+import pymongo
+import sys
+
+#Based on example code from MongoDB
+try:
+  client = pymongo.MongoClient(mongodb+srv://oestekevin:nbFFcWwiS4OYx5k4@sportsequipmentserver.xsphcuo.mongodb.net/?retryWrites=true&w=majority&appName=sportsEquipmentServer)
+  
+# return a friendly error if a URI error is thrown 
+except pymongo.errors.ConfigurationError:
+  print("Error: Could not connect to MongoDB.")
+  sys.exit(1)
+
+# use a database named "myDatabase"
+db = client.myDatabase
+
+# use a collection named "recipes"
+my_collection = db["inventory"]
+
+inventory_documents = []
+
+# drop the collection in case it already exists
+try:
+  my_collection.drop()  
+
+# return a friendly error if an authentication error is thrown
+except pymongo.errors.OperationFailure:
+  print("An authentication error was received. Are your username and password correct in your connection string?")
+  sys.exit(1)
+#add inventory to database
+try: 
+ result = my_collection.insert_many(inventory_documents)
+
+# return a friendly error if the operation fails
+except pymongo.errors.OperationFailure:
+  print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
+  sys.exit(1)
+else:
+  inserted_count = len(result.inserted_ids)
+  print("Inserted %x items." %(inserted_count))
+
+  print("\n")
