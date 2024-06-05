fdbdef0cf97b65696dc4d5e14803024ee5274128
kevin-oeste <oestekevin@gmail.com>
2024-06-05 12:41:38 -0400

    Update app.py
    Added connection to mongoDB through app.py, need to do more research before proceeding.

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template, request
-import pymongo as mongo
+import pymongo 
+from pymongo import MongoClient as mc
+
+#connect to database
+try:
+    uri = "mongodb+srv://oestekevin:nbFFcWwiS4OYx5k4@sportsequipmentserver.xsphcuo.mongodb.net/?retryWrites=true&w=majority&appName=sportsEquipmentServer"
+    client = mc(uri)
+    
+    database = client["sportsEquipmentServer"]
+    collection = database["ShoppingCart"]
+
+    #Replace with code
+    
+    client.close()
+except Exception as e:
+    raise Exception("The following error occured: ", e)
+    
 
 app = Flask(__name__)
 
@@ -20,3 +36,4 @@ def itemAdd():
             cat = request.form['itemCategory']
             stock = request.form['itemStock']
             price = request.form['itemPrice']
+            
