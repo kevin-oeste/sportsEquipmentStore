b8f0e342804d220e7fb75be0989f03a66b0b8bde
kevin-oeste <oestekevin@gmail.com>
2024-06-10 21:42:22 -0400

    Update inventoryManagement.py
    Adding more to the classes

--- inventoryManagement.py
+++ inventoryManagement.py
@@ -1,4 +1,7 @@
+#ideally the IDs should auto-increment, or we could 
 lastItemId = 0
+lastCustomerId = 0
+lastOrderId = 0
 class Item:
     #Primary Key (changed name from productId to itemId)
     int itemId
@@ -8,7 +11,7 @@ class Item:
     int stock
     double price
 
-    def Item():
+    def __init__(self):
         #Temporary implementation, but we will need to have an auto-incrementing ID number or use something like a GUID
         itemId = lastItemId + 1
         lastItemId = itemId
@@ -35,6 +38,56 @@ class shoppingCart:
     #foreign key (Order)
     int orderId
     #foreign key (Item)
-    int itemId
-    int numInCart
+    #int itemId
+    #int numInCart
+    def __init__(self):
+        cartItems = [(Item, 1)]
+    def addItemToCart(Item i):
+        cartItems.append(i)
+    
+
+class customer:
+    #primary key
+    int customerId
+    string name
+    string address
+    string email
+    string phone
+    def __init__(self):
+        customerId = lastCustomerId + 1
+        lastCustomerId = customerId
+        name = "John Doe"
+        address = "123 Road Way"
+        email = "johndoe@email.com"
+        phone = "123-456-7890"
+    def customer(string name1, string address1, string email1, string phone1):
+        customer tempCustomer
+        tempCustomer.name = name1
+        tempCustomer.address = address1
+        tempCustomer.email = email1
+        tempCustomer.phone = phone1
+        return tempCustomer
+class order:
+    #primary key
+    int orderId
+    #foreign key customer(customerId)
+    int customerId
+    double total
+    #datetime
+    string datePlaced
+    shoppingCart cart
+    def __init__(self):
+        orderId = lastOrderId + 1
+        lastOrderId = orderId
+        customerId = 0
+    def createOrder(customer c):
+        customerId = c.customerId
+        
+        
+
+class shipping:
+    string trackingNo
+    #foreign key order(orderId)
+    int orderId
+        
     
